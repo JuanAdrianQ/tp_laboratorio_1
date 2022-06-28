55f0ed26a9e84f85501b83c179972fb7bb0d300d
Adri4455 <adriquiroz000@gmail.com>
2022-06-28 15:13:25 -0300

    TP3-RECUPERATORIO

--- tp3_windows/Controller.c
+++ tp3_windows/Controller.c
@@ -59,7 +59,10 @@ int controller_loadFromBinary(char* path , LinkedList* pArrayListPassenger)
 		{
 			if(parser_PassengerFromBinary(pArchivo,pArrayListPassenger)== 0)
 			{
+<<<<<<< HEAD
 				printf("Hola");
+=======
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 				retorno = 0;
 			}
 		}
@@ -195,10 +198,17 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 
 		if(lenLinke > 0)
 		{
+<<<<<<< HEAD
 			if(utn_getNumero(&id,"Ingrese el id del pasajero que desea modificar: ","Error.El id ingresado no es valido.",1,lenLinke,5)==0)
 			{
 				idAModificar = Passenger_find(pArrayListPassenger,id);
 				if(idAModificar > 0)
+=======
+			if(utn_getNumero(&id,"\nIngrese el id del pasajero que desea modificar: ","\nError.El id ingresado no es valido.",1,lenLinke,5)==0)
+			{
+				idAModificar = Passenger_find(pArrayListPassenger,id);
+				if(idAModificar >= 0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 				{
 					pasajeroReemplazo = ll_get(pArrayListPassenger,idAModificar);
 
@@ -208,7 +218,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 						Passenger_printOne(pasajeroReemplazo);
 						do
 						{
+<<<<<<< HEAD
 							printf("\nMenu de opciones.Modificacion\n"
+=======
+							printf("\nMenu de opciones Modificacion\n"
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 									"1-Nombre\n"
 									"2-Apellido\n"
 									"3-Precio de vuelo\n"
@@ -221,7 +235,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 							switch(menuOpcionesModificacion)
 							{
 							case 1:
+<<<<<<< HEAD
 								if(utn_getString(nombre,"Ingrese su nuevo nombre: ","Error.Ingrese un nombre valido.")== 0)
+=======
+								if(utn_getString(nombre,"\nIngrese su nuevo nombre: ","\nError.Ingrese un nombre valido.")== 0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									if(Passenger_setNombre(pasajeroReemplazo,nombre)==0)
 									{
@@ -234,7 +252,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 								}
 								break;
 							case 2:
+<<<<<<< HEAD
 								if(utn_getString(apellido,"Ingrese su nuevo apellido: ","Error.Ingrese un apellido valido.")==0)
+=======
+								if(utn_getString(apellido,"\nIngrese su nuevo apellido: ","\nError.Ingrese un apellido valido.")==0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									if(Passenger_setApellido(pasajeroReemplazo,apellido)==0)
 									{
@@ -247,7 +269,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 								}
 								break;
 							case 3:
+<<<<<<< HEAD
 								if(utn_getFloat(precio,"Ingrese el nuevo precio: ","Error.Ingrese un precio valido.",1,9999999,5)==0)
+=======
+								if(utn_getFloat(precio,"\nIngrese el nuevo precio: ","\nError.Ingrese un precio valido.",1,9999999,5)==0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									if(Passenger_setPrecio(pasajeroReemplazo,atof(precio))==0)
 									{
@@ -260,7 +286,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 								}
 								break;
 							case 4:
+<<<<<<< HEAD
 								if(utn_getNumero(&tipoPasajero,"Ingrese su nuevo tipo de pasajero(1- First Class,2- Executive Class,3- Economy Class: ","Error.Ingrese un tipo de pasajer valido",1,3,5)== 0)
+=======
+								if(utn_getNumero(&tipoPasajero,"\nIngrese su nuevo tipo de pasajero(1- First Class,2- Executive Class,3- Economy Class: ","\nError.Ingrese un tipo de pasajer valido",1,3,5)== 0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									if(Passenger_setTipoPasajero(pasajeroReemplazo,tipoPasajero)==0)
 									{
@@ -273,7 +303,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 								}
 								break;
 							case 5:
+<<<<<<< HEAD
 								if(utn_getString(codigoVuelo,"Ingrese el codigo de vuelo: ","Error.Ingrese un codigo de vuelo valido.")==0)
+=======
+								if(utn_getString(codigoVuelo,"\nIngrese el codigo de vuelo: ","\nError.Ingrese un codigo de vuelo valido.")==0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									if(Passenger_setCodigoVuelo(pasajeroReemplazo,codigoVuelo)==0)
 									{
@@ -286,7 +320,11 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 								}
 								break;
 							case 6:
+<<<<<<< HEAD
 								if(utn_getNumero(&estadoVuelo,"Ingrese su nuevo estado de vuelo(1- Aterrizado,2- En horario,3- En vuelo,-4 Demorado: ","Error.Ingrese un estado de vuelo valido.",1,4,5)==0)
+=======
+								if(utn_getNumero(&estadoVuelo,"\nIngrese su nuevo estado de vuelo(1- Aterrizado,2- En horario,3- En vuelo,-4 Demorado: ","\nError.Ingrese un estado de vuelo valido.",1,4,5)==0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 								{
 									switch(estadoVuelo)
 									{
@@ -327,6 +365,13 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
 						}while(menuOpcionesModificacion != 7);
 					}
 				}
+<<<<<<< HEAD
+=======
+				else
+				{
+					retorno = -1;
+				}
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 			}
 		}
 	}
@@ -334,7 +379,10 @@ int controller_editPassenger(LinkedList* pArrayListPassenger)
     return retorno;
 
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 /** \brief Baja de pasajero
  *
  * \param path char*
@@ -358,20 +406,37 @@ int controller_removePassenger(LinkedList* pArrayListPassenger)
 
 		if(lenLinke > 0)
 		{
+<<<<<<< HEAD
 			if(utn_getNumero(&id,"Ingrese el id del pasajero que quiere modificar: ","Error.Ingrese un id valido.",1,lenLinke,5)==0)
 			{
 				indiceAModificar = Passenger_find(pArrayListPassenger,id);
 				if(indiceAModificar > 0)
+=======
+			if(utn_getNumero(&id,"\nIngrese el id del pasajero que quiere eliminar: ","\nError.Ingrese un id valido.",1,lenLinke,5)==0)
+			{
+				indiceAModificar = Passenger_find(pArrayListPassenger,id);
+				if(indiceAModificar >= 0)
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 				{
 					pasajeroAEliminar = ll_get(pArrayListPassenger,indiceAModificar);
 					if(pasajeroAEliminar != NULL)
 					{
+<<<<<<< HEAD
 						printf("El pasajero que se va a dar de baja es: ");
 						Passenger_printOne(pasajeroAEliminar);
 
 						printf("Seguro que desea darlo de baja"
 								"1- Si"
 								"2- No");
+=======
+						printf("\nEl pasajero que se va a dar de baja es: ");
+						Passenger_printOne(pasajeroAEliminar);
+
+						printf("\nSeguro que desea darlo de baja\n"
+								"1- Si\n"
+								"2- No\n"
+								"Elija una opcion: ");
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 						scanf("%d",&MenuBaja);
 						switch(MenuBaja)
 						{
@@ -417,11 +482,24 @@ int controller_ListPassenger(LinkedList* pArrayListPassenger)
 
 		cantElementosPasajeros = ll_len(pArrayListPassenger);
 
+<<<<<<< HEAD
 		for(int i = 0; i < cantElementosPasajeros; i++)
 		{
 			unPasajero = (Passenger*) ll_get(pArrayListPassenger, i);
 			Passenger_printOne(unPasajero);
 		}
+=======
+		if(cantElementosPasajeros > 0)
+		{
+			for(int i = 0; i < cantElementosPasajeros;i++)
+			{
+				unPasajero = (Passenger*) ll_get(pArrayListPassenger, i);
+				Passenger_printOne(unPasajero);
+			}
+			retorno = 0;
+		}
+
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 	}
 
     return retorno;
@@ -440,11 +518,15 @@ int controller_sortPassenger(LinkedList* pArrayListPassenger)
 	int opcionesOrden;
 	int orden;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 	if(pArrayListPassenger != NULL)
 	{
 		do
 		{
+<<<<<<< HEAD
 			printf("Opciones para ordenar el pasajero"
 					"1-Ordenar por id"
 					"2-Ordenar por Apellido"
@@ -453,6 +535,16 @@ int controller_sortPassenger(LinkedList* pArrayListPassenger)
 					"Ingrese como desea ordenar: ");
 			scanf("%d",&opcionesOrden);
 			utn_getNumero(&orden,"Ingrese tipo de ordenamiento(1-Ascendente,0-Descendente","Error.Ingrese un tipo de ordenamiento valido.",0,1,5);
+=======
+			printf("\nOpciones para ordenar el pasajero\n"
+					"\n1-Ordenar por id\n"
+					"2-Ordenar por Apellido\n"
+					"3-Ordenar por tipo de pasajero\n"
+					"4-Ordenar por codigo de vuelo\n"
+					"Ingrese como desea ordenar: ");
+			scanf("%d",&opcionesOrden);
+			utn_getNumero(&orden,"\nIngrese tipo de ordenamiento(1-Ascendente,0-Descendente","\nError.Ingrese un tipo de ordenamiento valido.",0,1,5);
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 
 			switch(opcionesOrden)
 			{
@@ -481,7 +573,11 @@ int controller_sortPassenger(LinkedList* pArrayListPassenger)
 				}
 				break;
 			default:
+<<<<<<< HEAD
 					printf("Ingrese un tipo de orden valido");
+=======
+					printf("\nIngrese un tipo de orden valido\n");
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 				break;
 			}
 		}while(opcionesOrden != 4);
@@ -500,7 +596,85 @@ int controller_sortPassenger(LinkedList* pArrayListPassenger)
  */
 int controller_saveAsText(char* path , LinkedList* pArrayListPassenger)
 {
+<<<<<<< HEAD
     return 1;
+=======
+	int retorno = -1;
+
+	if(path != NULL && pArrayListPassenger != NULL)
+	{
+		FILE* pArchivo;
+		Passenger* pasajeroAux;
+		int len;
+		int i;
+		int idAux;
+		char nombre[TAM_CHAR];
+		char apellido[TAM_CHAR];
+		float precio;
+		int tipoPasajero;
+		char tipoPasajeroChar[TAM_CHAR];
+		char codigoVuelo[TAM_CHAR];
+		char estadoVuelo[TAM_CHAR];
+
+		len = ll_len(pArrayListPassenger);
+
+		pArchivo = fopen(path,"w");
+
+		if(pArchivo != NULL)
+		{
+			for(i = 0;i < len;i++)
+			{
+				pasajeroAux = ll_get(pArrayListPassenger,i);
+				if(pasajeroAux != NULL)
+				{
+					if(Passenger_getId(pasajeroAux,&idAux)==0)
+					{
+						if(Passenger_getNombre(pasajeroAux,nombre)==0)
+						{
+							if(Passenger_getApellido(pasajeroAux,apellido)==0)
+							{
+								if(Passenger_getPrecio(pasajeroAux,&precio)==0)
+								{
+									if(Passenger_getTipoPasajero(pasajeroAux,&tipoPasajero)==0)
+									{
+										if(tipoPasajero == 1)
+										{
+											strcpy(tipoPasajeroChar,"FirstClass");
+										}
+										else
+										{
+											if(tipoPasajero == 2)
+											{
+												strcpy(tipoPasajeroChar,"ExecutiveClass");
+											}
+											else
+											{
+												strcpy(tipoPasajeroChar,"EconomyClass");
+											}
+										}
+									}
+									if(Passenger_getCodigoVuelo(pasajeroAux,codigoVuelo)==0)
+									{
+										if(Passenger_getEstadoVuelo(pasajeroAux,estadoVuelo)==0)
+										{
+											fprintf(pArchivo,"%d,%s,%s,%f,%s %s %s\n",idAux,nombre,apellido,precio,codigoVuelo,tipoPasajeroChar,estadoVuelo);
+											retorno = 0;
+										}
+									}
+
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+		fclose(pArchivo);
+
+	}
+    return retorno;
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 }
 
 /** \brief Guarda los datos de los pasajeros en el archivo data.csv (modo binario).
@@ -512,6 +686,40 @@ int controller_saveAsText(char* path , LinkedList* pArrayListPassenger)
  */
 int controller_saveAsBinary(char* path , LinkedList* pArrayListPassenger)
 {
+<<<<<<< HEAD
     return 1;
 }
 
+=======
+	int retorno = -1;
+	int i;
+	int len;
+
+	len = ll_len(pArrayListPassenger);
+
+	if(path != NULL && pArrayListPassenger != NULL)
+	{
+		FILE* pArchivo;
+		Passenger* pasajeroAux;
+
+		pArchivo = fopen(path,"wb");
+
+		if(pArchivo != NULL)
+		{
+			for(i = 0;i < len;i++)
+			{
+				pasajeroAux = ll_get(pArrayListPassenger,i);
+				if(pasajeroAux != NULL)
+				{
+					fwrite(pasajeroAux,sizeof(Passenger),1,pArchivo);
+
+				}
+			}
+			retorno = 0;
+		}
+		fclose(pArchivo);
+
+	}
+    return retorno;
+}
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
--- tp3_windows/main.c
+++ tp3_windows/main.c
@@ -4,8 +4,11 @@
 #include "Controller.h"
 #include "parser.h"
 #include "Passenger.h"
+<<<<<<< HEAD
 #include "LinkedList.h"
 
+=======
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
 
 /****************************************************
     Menu:
@@ -30,7 +33,14 @@ int main()
 	int opcion = 0;
 	int salidaBaja;
 	int salidaOrdenamiento;
+<<<<<<< HEAD
 
+=======
+	int bajaOrdenar = 0;
+	int contadorCargaTexto = 0;
+	int contadorCargaBinario = 0;
+	int cargaDeDatos = 0;
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
     LinkedList* listaPasajeros = ll_newLinkedList();
 
     do{
@@ -51,6 +61,7 @@ int main()
         switch(opcion)
         {
             case 1:
+<<<<<<< HEAD
             	if(controller_loadFromText("data.csv",listaPasajeros)==0)
             	{
             		printf("Se cargaron los datos de pasajeros en modo texto con exito.");
@@ -68,6 +79,44 @@ int main()
             	else
             	{
             		printf("Error");
+=======
+            	if(cargaDeDatos == 0)
+            	{
+            		if(controller_loadFromText("data.csv",listaPasajeros)==0)
+					{
+						printf("\nSe cargaron los datos de pasajeros en modo texto con exito.\n");
+						contadorCargaTexto++;
+						cargaDeDatos++;
+					}
+					else
+					{
+						printf("\nNo se pudo cargar los datos debido a un error.\n");
+					}
+
+            	}
+            	else
+            	{
+            		printf("\nYa cargo los datos de pasajeros en modo texto.\n");
+            	}
+            	break;
+            case 2:
+            	if(cargaDeDatos == 0)
+            	{
+            		if(controller_loadFromBinary("data.bin",listaPasajeros)==0)
+					{
+						printf("\nSe cargaron los datos de pasajeros en modo binario con exito.\n");
+						contadorCargaBinario++;
+						cargaDeDatos++;
+					}
+					else
+					{
+						printf("\nError\n");
+					}
+            	}
+            	else
+            	{
+            		printf("\nYa cargo los datos de los pasajeros en modo binario.\n");
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
             	}
             	break;
             case 3: //Alta de pasajero
@@ -75,6 +124,10 @@ int main()
             	if(controller_addPassenger(listaPasajeros)==0)
             	{
             		printf("\nSe agrego un pasajero exitosamente.\n");
+<<<<<<< HEAD
+=======
+            		bajaOrdenar++;
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
             	}
             	else
             	{
@@ -82,6 +135,7 @@ int main()
             	}
             	break;
             case 4: //Modificar pasajero
+<<<<<<< HEAD
             	if(controller_editPassenger(listaPasajeros)==0)
             	{
             		printf("\nSe modifico correctamente al pasajero.\n");
@@ -135,6 +189,105 @@ int main()
             	break;
             case 8: //Guardar los datos de pasajeros en modo texto
 
+=======
+            	if(bajaOrdenar > 0)
+            	{
+            		if(controller_editPassenger(listaPasajeros)==0)
+					{
+						printf("\nSe modifico correctamente al pasajero.\n");
+					}
+					else
+					{
+						printf("\nNo se pudo modificar al pasajero.\n");
+					}
+            	}
+            	else
+            	{
+            		printf("\nPorfavor debe dar de alta un pasajero!!!\n");
+            	}
+            	break;
+            case 5: // Baja de pasajero
+            if(bajaOrdenar > 0)
+            {
+            	salidaBaja = controller_removePassenger(listaPasajeros);
+
+				if(salidaBaja == 0)
+				{
+					printf("\nSe a eliminado al pasajero del sistema con exito\n");
+				}
+				else
+				{
+					if(salidaBaja == -1)
+					{
+						printf("\nOcurrio un error al intentar eliminar al pasajero\n");
+					}
+					else
+					{
+						printf("\nSe decidio no eliminar al pasajero.\n");
+					}
+				}
+            }
+            else
+            {
+            	printf("\nPorfavor debe dar de alta un pasajero!!!\n");
+            }
+            break;
+            case 6:
+			if(contadorCargaTexto > 0 || contadorCargaBinario > 0 || bajaOrdenar > 0)
+			{
+				if(controller_ListPassenger(listaPasajeros)==0)
+				{
+					printf("\nLista de pasajeros mostrada con exito.\n");
+				}
+				else
+				{
+					printf("\nOcurrio un error al intentar listar los pasajeros.\n");
+				}
+			}
+			else
+			{
+				printf("\nNo hay pasajeros para listar.\n");
+			}
+            break;
+            case 7: //Ordenar pasajeros
+            if(bajaOrdenar > 0)
+            {
+            	salidaOrdenamiento = controller_sortPassenger(listaPasajeros);
+				switch(salidaOrdenamiento)
+				{
+				case 1:
+					printf("\nSe ordeno los pasajeros por id con exito\n");
+					break;
+				case 2:
+					printf("\nSe ordeno los pasajeros por apellido con exito\n");
+					break;
+				case 3:
+					printf("\nSe ordeno los pasajeros por tipo de pasajero con exito\n");
+					break;
+				case 4:
+					printf("\nSe ordeno los pasajeros por codigo de vuelo con exito\n");
+					break;
+				default:
+					printf("\nOcurrio un error al intentar ordenar los pasajeros\n");
+					break;
+				}
+            }
+            else
+            {
+            	printf("\nPorfavor debe dar de alta un pasajero!!!\n");
+            }
+            break;
+            case 8: //Guardar los datos de pasajeros en modo texto
+
+            	if(controller_saveAsText("data.csv",listaPasajeros)==0)
+            	{
+            		printf("\nSe guardo con exito los datos de los pasajeros del archivo data.csv\n");
+            	}
+            	else
+            	{
+            		printf("\nOcurrio un error al intentar guardar los datos de los pasajeros archivo data.csv\n");
+            	}
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
             	break;
             case 9://Guardar los datos de pasajeros en modo binario
 
@@ -148,7 +301,10 @@ int main()
     }while(opcion != 10);
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2f8a6c5 (TP3-Recuperatorio)
     return 0;
 }
 
